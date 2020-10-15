e5bffadedaa3843575294aac048bece5a6f48cd1
Dan Gautsch <dgautsch@users.noreply.github.com>
2020-10-14 22:32:20 -0400

    Add type definitions to state machine project

---
+++ @types/state-machine.d.ts
@@ -0,0 +1,32 @@
+// Type definitions for state machine 0.2.2
+// Project: https://github.com/brophdawg11/state-machine#readme
+// TypeScript Version: 3.7.4
+
+export default StateMachine
+
+declare class StateMachine {
+    constructor(machine: MachineDefinition, initialState?: string);
+    currentState(): string;
+    getDotFile(): string;
+    transition(transition: string, data?:any):Promise<null>
+}
+
+export type MachineDefinition = {
+    states: StateCollection,
+    onEnter?: OnStateEnter
+}
+
+type OnStateEnter = (payload: any, newState: any, oldState: oldState) => void;
+
+type StateCollection = {
+    [key: string]: StateDefinition
+};
+
+type StateDefinition = {
+    transitions: Transition,
+    onEnter?: OnStateEnter
+};
+
+type Transition = {
+    [key: string]: string
+}
