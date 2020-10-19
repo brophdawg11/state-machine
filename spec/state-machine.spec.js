7f9ed99bf37f6dcfe77fdf29579755d474da9e07
Matt Brophy <matt@brophy.org>
2020-10-19 10:53:52 -0400

    Update eslint to latest

--- spec/state-machine.spec.js
+++ spec/state-machine.spec.js
@@ -195,7 +195,7 @@ describe('StateMachine', () => {
                         started: 'on',
                     },
                     onEnter() {
-                        return new Promise(resolve => setTimeout(() => {
+                        return new Promise((resolve) => setTimeout(() => {
                             machine.transition('started');
                             resolve('done starting');
                         }, 1000));
